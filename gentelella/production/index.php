6cfdd49af383605fe83f8b2103cc7141b089e2e4
ChrisGiuffrida <chris@gonotredame.net>
2017-12-05 23:10:02 -0500

    User Page

--- gentelella/production/index.php
+++ gentelella/production/index.php
@@ -250,7 +250,7 @@ function print_ticker($change) {
                                 </div>
                               </div>
                               <div class="pricing_footer">
-                                <a href="javascript:void(0);" class="btn btn-success btn-block" role="button">More Info <span> now!</span></a>
+                                <a href="student00.php" class="btn btn-success btn-block" role="button">More Info <span></span></a>
                                 <p>
                                 </p>
                               </div>
@@ -261,23 +261,48 @@ function print_ticker($change) {
 
                         <!-- price element -->
                         <div class="col-md-3 col-sm-6 col-xs-12">
-                          <div class="pricing ui-ribbon-container">
-                            <div class="ui-ribbon-wrapper">
-                              <div class="ui-ribbon">
-                                30% Off
+                        <div class="pricing">
+                          <div class="title">
+                            <h2></h2>
+                            <h1>student01</h1>
+                          </div>
+                          <div class="x_content">
+                            <div class="">
+                              <div class="pricing_features">
+                                <ul class="list-unstyled text-left">
+                                  <li><i class="fa fa-times text-danger"></i> 2 years access <strong> to all storage locations</strong></li>
+                                  <li><i class="fa fa-times text-danger"></i> <strong>Unlimited</strong> storage</li>
+                                  <li><i class="fa fa-check text-success"></i> Limited <strong> download quota</strong></li>
+                                  <li><i class="fa fa-check text-success"></i> <strong>Cash on Delivery</strong></li>
+                                  <li><i class="fa fa-check text-success"></i> All time <strong> updates</strong></li>
+                                  <li><i class="fa fa-times text-danger"></i> <strong>Unlimited</strong> access to all files</li>
+                                  <li><i class="fa fa-times text-danger"></i> <strong>Allowed</strong> to be exclusing per sale</li>
+                                </ul>
                               </div>
                             </div>
+                            <div class="pricing_footer">
+                              <a href="student01.php" class="btn btn-success btn-block" role="button">More Info <span></span></a>
+                              <p>
+                              </p>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <!-- price element -->
+
+                      <!-- price element -->
+                      <div class="col-md-3 col-sm-6 col-xs-12">
+                          <div class="pricing">
                             <div class="title">
-                              <h2>Tally Box Design</h2>
-                              <h1>$25</h1>
-                              <span>Monthly</span>
+                              <h2></h2>
+                              <h1>student02</h1>
                             </div>
                             <div class="x_content">
                               <div class="">
                                 <div class="pricing_features">
                                   <ul class="list-unstyled text-left">
-                                    <li><i class="fa fa-check text-success"></i> 2 years access <strong> to all storage locations</strong></li>
-                                    <li><i class="fa fa-check text-success"></i> <strong>Unlimited</strong> storage</li>
+                                    <li><i class="fa fa-times text-danger"></i> 2 years access <strong> to all storage locations</strong></li>
+                                    <li><i class="fa fa-times text-danger"></i> <strong>Unlimited</strong> storage</li>
                                     <li><i class="fa fa-check text-success"></i> Limited <strong> download quota</strong></li>
                                     <li><i class="fa fa-check text-success"></i> <strong>Cash on Delivery</strong></li>
                                     <li><i class="fa fa-check text-success"></i> All time <strong> updates</strong></li>
@@ -287,9 +312,8 @@ function print_ticker($change) {
                                 </div>
                               </div>
                               <div class="pricing_footer">
-                                <a href="javascript:void(0);" class="btn btn-primary btn-block" role="button">Download <span> now!</span></a>
+                                <a href="student02.php" class="btn btn-success btn-block" role="button">More Info <span></span></a>
                                 <p>
-                                  <a href="javascript:void(0);">Sign up</a>
                                 </p>
                               </div>
                             </div>
@@ -301,16 +325,15 @@ function print_ticker($change) {
                         <div class="col-md-3 col-sm-6 col-xs-12">
                           <div class="pricing">
                             <div class="title">
-                              <h2>Tally Box Design</h2>
-                              <h1>$25</h1>
-                              <span>Monthly</span>
+                              <h2></h2>
+                              <h1>student04</h1>
                             </div>
                             <div class="x_content">
                               <div class="">
                                 <div class="pricing_features">
                                   <ul class="list-unstyled text-left">
-                                    <li><i class="fa fa-check text-success"></i> 2 years access <strong> to all storage locations</strong></li>
-                                    <li><i class="fa fa-check text-success"></i> <strong>Unlimited</strong> storage</li>
+                                    <li><i class="fa fa-times text-danger"></i> 2 years access <strong> to all storage locations</strong></li>
+                                    <li><i class="fa fa-times text-danger"></i> <strong>Unlimited</strong> storage</li>
                                     <li><i class="fa fa-check text-success"></i> Limited <strong> download quota</strong></li>
                                     <li><i class="fa fa-check text-success"></i> <strong>Cash on Delivery</strong></li>
                                     <li><i class="fa fa-check text-success"></i> All time <strong> updates</strong></li>
@@ -320,9 +343,8 @@ function print_ticker($change) {
                                 </div>
                               </div>
                               <div class="pricing_footer">
-                                <a href="javascript:void(0);" class="btn btn-success btn-block" role="button">Download <span> now!</span></a>
+                                <a href="student00.php" class="btn btn-success btn-block" role="button">More Info <span></span></a>
                                 <p>
-                                  <a href="javascript:void(0);">Sign up</a>
                                 </p>
                               </div>
                             </div>
@@ -330,38 +352,9 @@ function print_ticker($change) {
                         </div>
                         <!-- price element -->
 
-                        <!-- price element -->
-                        <div class="col-md-3 col-sm-6 col-xs-12">
-                          <div class="pricing">
-                            <div class="title">
-                              <h2>Tally Box Design</h2>
-                              <h1>$25</h1>
-                              <span>Monthly</span>
-                            </div>
-                            <div class="x_content">
-                              <div class="">
-                                <div class="pricing_features">
-                                  <ul class="list-unstyled text-left">
-                                    <li><i class="fa fa-check text-success"></i> 2 years access <strong> to all storage locations</strong></li>
-                                    <li><i class="fa fa-check text-success"></i> <strong>Unlimited</strong> storage</li>
-                                    <li><i class="fa fa-check text-success"></i> Limited <strong> download quota</strong></li>
-                                    <li><i class="fa fa-check text-success"></i> <strong>Cash on Delivery</strong></li>
-                                    <li><i class="fa fa-check text-success"></i> All time <strong> updates</strong></li>
-                                    <li><i class="fa fa-check text-success"></i> <strong>Unlimited</strong> access to all files</li>
-                                    <li><i class="fa fa-check text-success"></i> <strong>Allowed</strong> to be exclusing per sale</li>
-                                  </ul>
-                                </div>
-                              </div>
-                              <div class="pricing_footer">
-                                <a href="javascript:void(0);" class="btn btn-success btn-block" role="button">Download <span> now!</span></a>
-                                <p>
-                                  <a href="javascript:void(0);">Sign up</a>
-                                </p>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                        <!-- price element -->
+                        
+
+                        
                       </div>
                     </div>
                   </div>
