085d99e1a8b63cf5e114a3aa04561b4327367aba
ChrisGiuffrida <chris@gonotredame.net>
2017-12-05 23:11:27 -0500

    User Page

--- gentelella/production/index.php
+++ gentelella/production/index.php
@@ -190,15 +190,7 @@ function print_ticker($change) {
         <!-- page content -->
         <div class="right" role="main">
           <div class="">
-            <div class="page-title">
-              <div class="title_left">
-              </div>
-
-            
-            </div>
-
             <div class="clearfix"></div>
-
             <div class="row">
               <div class="col-md-12 col-sm-12 col-xs-12">
                 <div class="x_panel" style="height:600px;">
